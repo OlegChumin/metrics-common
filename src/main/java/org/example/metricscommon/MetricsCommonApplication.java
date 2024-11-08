5f2c776b28c701cf2502d096f25fef9278d781d0
Oleg Chumin <tschumin.oleg@gmail.com>
2024-11-08 10:22:30 +0300

    Initial commit

---
+++ src/main/java/org/example/metricscommon/MetricsCommonApplication.java
@@ -0,0 +1,13 @@
+package org.example.metricscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsCommonApplication.class, args);
+    }
+
+}
