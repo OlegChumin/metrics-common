3b7e78e1f2c62c47e6d2b7de873caac9a2db368f
Oleg.Chumin <oleg.tschumin@gmail.com>
2024-11-29 16:01:36 +0300

    feat: TEAMEPB-410: удален неиспользуемый импорт

--- src/main/java/org/example/metrics/common/config/MetricConfiguration.java
+++ src/main/java/org/example/metrics/common/config/MetricConfiguration.java
@@ -2,16 +2,13 @@ package org.example.metrics.common.config;
 
 import io.opentracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
-import org.example.metrics.common.aspect.jaeger.JaegerHTTPTracingRestTemplateInterceptor;
+
 import org.example.metrics.common.aspect.jaeger.JaegerHttpTracingExtractorNew;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.client.RestTemplate;
-
 
 import javax.annotation.PostConstruct;
 
