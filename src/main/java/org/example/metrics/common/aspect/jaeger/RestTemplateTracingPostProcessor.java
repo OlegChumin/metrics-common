7cc77368efbfad7fb32d062c747286013812bf44
Oleg.Chumin <oleg.tschumin@gmail.com>
2024-12-04 11:32:07 +0300

    feat: TEAMEPB-410: интрецептор принудительно добавляется в конец списка для предотвращения изменения заголовков другими интерцепторами

--- src/main/java/org/example/metrics/common/aspect/jaeger/RestTemplateTracingPostProcessor.java
+++ src/main/java/org/example/metrics/common/aspect/jaeger/RestTemplateTracingPostProcessor.java
@@ -23,19 +23,23 @@ public class RestTemplateTracingPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-
-        // Проверка наличия JaegerHTTPTracingRestTemplateInterceptor
         if (bean instanceof RestTemplate restTemplate) {
             List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>(restTemplate.getInterceptors());
-            // Проверяем, есть ли уже трейсинг-интерсептор
-            if (interceptors.stream()
-                    .noneMatch(interceptor -> interceptor instanceof JaegerHTTPTracingRestTemplateInterceptor)) {
-                interceptors.add(new JaegerHTTPTracingRestTemplateInterceptor(tracer));
-                restTemplate.setInterceptors(interceptors); // Устанавливаем обновленный список
-                log.info("JaegerHTTPTracingRestTemplateInterceptor added to RestTemplate bean: {}", beanName);
-            }
+
+            // Удаляем уже существующий экземпляр нашего интерцептора, если он есть
+            interceptors.removeIf(interceptor -> interceptor instanceof JaegerHTTPTracingRestTemplateInterceptor);
+
+            // Добавляем наш интерцептор в конец списка
+            interceptors.add(new JaegerHTTPTracingRestTemplateInterceptor(tracer));
+
+            // Устанавливаем обновлённый список интерцепторов
+            restTemplate.setInterceptors(interceptors);
+
+            // Логируем факт добавления интерцептора
+            log.info("JaegerHTTPTracingRestTemplateInterceptor added to RestTemplate bean: {}", beanName);
         }
         return bean;
     }
+
 
 }
